5c2e284b822d2041c1bb5cd1af13de1bffb117cc
Emil Guseynov <emilgemilgemilg@gmail.com>
2023-03-03 19:15:24 +0400

    added ratingView and saveButton

--- BookRecipes/Module/Saved/SavedTableCell.swift
+++ BookRecipes/Module/Saved/SavedTableCell.swift
@@ -11,6 +11,7 @@ import SDWebImage
 class SavedTableCell: UITableViewCell {
     
     static let reuseId = "SavedTableCell"
+
     let inset: CGFloat = 16
     
     lazy var cellImageView: UIImageView = {
@@ -30,11 +31,21 @@ class SavedTableCell: UITableViewCell {
         return label
     }()
     
+    lazy var ratingView = RatingView()
+    lazy var saveButton = SaveButton()
+    
+    var saveButtonClosure: (() -> ())?
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
         contentView.addSubview(cellImageView)
         contentView.addSubview(nameLabel)
+        contentView.addSubview(ratingView)
+        contentView.addSubview(saveButton)
+        
+        saveButton.addTarget(self, action: #selector(saveButtonTapped), for: .touchUpInside)
+        
         backgroundColor = .clear
         
         constraints()
@@ -46,6 +57,7 @@ class SavedTableCell: UITableViewCell {
     
     private func constraints() {
         NSLayoutConstraint.activate([
+            
             cellImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: inset),
             cellImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: inset),
             cellImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -inset),
@@ -54,8 +66,23 @@ class SavedTableCell: UITableViewCell {
             nameLabel.topAnchor.constraint(equalTo: cellImageView.bottomAnchor, constant: 12),
             nameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: inset),
             nameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -inset),
-            nameLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -inset)
+            nameLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -inset),
+            
+            ratingView.topAnchor.constraint(equalTo: cellImageView.topAnchor, constant: 10),
+            ratingView.leadingAnchor.constraint(equalTo: cellImageView.leadingAnchor, constant: 10),
+            ratingView.heightAnchor.constraint(equalToConstant: 35),
+            ratingView.widthAnchor.constraint(equalToConstant: 70),
+            
+            saveButton.topAnchor.constraint(equalTo: cellImageView.topAnchor, constant: 10),
+            saveButton.trailingAnchor.constraint(equalTo: cellImageView.trailingAnchor, constant: -10),
+            saveButton.heightAnchor.constraint(equalToConstant: 35),
+            saveButton.widthAnchor.constraint(equalToConstant: 35),
         ])
+    }
+    
+    @objc func saveButtonTapped() {
+        
+        saveButtonClosure?()
     }
     
     func configure(with imageUrl: String, text: String) {
