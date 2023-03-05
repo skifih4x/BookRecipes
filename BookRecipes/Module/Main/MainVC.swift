db770275182dfe578ed5a7d29ab705d0cdbeed15
Tatsiana Gouteeva <gouteeva@gmail.com>
2023-03-05 17:14:08 +0300

    Remove unused code

--- BookRecipes/Module/Main/MainVC.swift
+++ BookRecipes/Module/Main/MainVC.swift
@@ -16,8 +16,8 @@ final class MainVC: UIViewController {
         return view
     }()
     
-    private var recipesModels: [SafeRecipe] = []
-    private var searchedRecipes: [SafeRecipe] = []
+    var recipesModels: [Recipe] = []
+    var searchedRecipes: [Recipe] = []
     
     private let apiManager = APICaller.shared
     private let mainTableView = MainTableView()
@@ -52,6 +52,11 @@ final class MainVC: UIViewController {
 
         
         //setup()
+    }
+    
+    func updateMainTableView() {
+        mainTableView.configure(models: searchedRecipes)
+        mainTableView.mainTableView.reloadData()
     }
 }
 
@@ -95,64 +100,9 @@ private extension MainVC {
         ])
     }
     
-    func updateMainTableView() {
-        mainTableView.configure(models: searchedRecipes)
-        mainTableView.mainTableView.reloadData()
-    }
-    
     func hideMainTableView(isTableViewHidden: Bool) {
         mainTableView.isHidden = isTableViewHidden
         mainView.isHidden = !isTableViewHidden
-    }
-    
-    func fetchData(for type: Types) {
-        let dispatchGroup = DispatchGroup()
-        APICaller.shared.getSortedRecipes(type: type) { results in
-            switch results {
-            case .success(let recipes):
-                // Успешно получено
-                for i in recipes {
-                    dispatchGroup.enter()
-                    APICaller.shared.getDetailedRecipe(with: i.id) { results in
-                        switch results {
-                        case .success(let recipe):
-                            print(recipe)
-                            // успешно получены детальные данные
-                            APICaller.shared.getImage(from: recipe.image!) { result in
-                                switch result {
-                                case .success(let imageData):
-                                    let safeRecipe = SafeRecipe(recipe: recipe, imageData: imageData)
-                                    self.recipesModels.append(safeRecipe)
-//                                    switch type {
-//                                    case .popular:
-//                                        self.mainView.popularRecipes.append(safeRecipe)
-//                                    case .healthy:
-//                                        self.mainView.healthyRecipes.append(safeRecipe)
-//                                    case .dessert:
-//                                        self.mainView.dessertRecipes.append(safeRecipe)
-//                                    }
-                                case .failure(let error):
-                                    print(error)
-                                }
-                                dispatchGroup.leave()
-                            }
-                        case .failure(let error):
-                            print(error)
-                            // получена ошибка при запросе детальных данных
-                            dispatchGroup.leave()
-                        }
-                    }
-                }
-                dispatchGroup.notify(queue: .main) {
-                    self.mainView.collectionView.reloadData()
-                    self.searchedRecipes = self.recipesModels
-                    self.updateMainTableView()
-                }
-            case .failure(let error):
-                print (error)
-                // получена ошибка
-            }
-        }
     }
 }
  
