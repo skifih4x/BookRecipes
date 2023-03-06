bf3484fb25ea404804403e8e799d904158c5f19d
cmdprm <2badhermit@gmail.com>
2023-03-06 12:49:15 +0300

    Feat: Add segue between CategoriesVC and RecipeListVC

--- BookRecipes/Helper/APICaller.swift
+++ BookRecipes/Helper/APICaller.swift
@@ -12,7 +12,7 @@ struct Constants {
     
     //static let APIKey = "7e31fd338a334d03aafda200f55348c0"
     //static let APIKey = "3632101b02674a0e97fb8b63eb12646e"
-    static let APIKey = "34f7a796b8c345a3abc987d7b2441e31"
+    static let APIKey = "c5579ba8b4734f44b80f0348e3a39505"
     //static let APIKey = "04864a53f7464c90820b2725af2c6ba0"
     //static let APIKey = "206625f6a74745dda3cb47905f129e1c"
     //static let APIKey = "bc63ec8984094be58b7a2d77da76b373"
